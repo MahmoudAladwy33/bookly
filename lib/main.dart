0f1a4939727f164c6217e6337a0f431e0858bdf2
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-29 13:04:04 +0300

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class Bookly extends StatelessWidget {
   const Bookly({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
