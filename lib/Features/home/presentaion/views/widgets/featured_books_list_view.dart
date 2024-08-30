25d76a4a15296e888e92dab94de75fb323c588c3
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-30 20:39:11 +0300

    create styles file & add title to home view

--- lib/Features/home/presentaion/views/widgets/featured_books_list_view.dart
+++ lib/Features/home/presentaion/views/widgets/featured_books_list_view.dart
@@ -7,20 +7,17 @@ class FeaturedBooksListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 190,
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .3,
-        child: ListView.builder(
-            itemCount: 5,
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              return const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: FeaturedListViewItem(),
-              );
-            }),
-      ),
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          itemCount: 5,
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: FeaturedListViewItem(),
+            );
+          }),
     );
   }
 }
--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/featured_books_list_view.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,9 +9,23 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
-        FeaturedBooksListView(),
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: FeaturedBooksListView(),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        )
       ],
     );
   }
