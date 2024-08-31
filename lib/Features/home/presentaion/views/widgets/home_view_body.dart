6d84a3cff70a27203705db2a060ffaece37832a5
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-31 12:45:10 +0300

    best seller list view item part one

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/featured_books_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -25,7 +26,8 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.titleMedium,
           ),
-        )
+        ),
+        BestSellerListViewItem(),
       ],
     );
   }
