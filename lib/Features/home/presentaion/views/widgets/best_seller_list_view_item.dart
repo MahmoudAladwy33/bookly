1c7ddbed6fd50f809ef7740a782e32c6066221c4
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-31 14:12:24 +0300

    best seller list view item part 2

--- lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
@@ -1,5 +1,7 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -8,7 +10,7 @@ class BestSellerListViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 125,
-      child: Row( 
+      child: Row(
         children: [
           AspectRatio(
             aspectRatio: 2.5 / 4,
@@ -21,7 +23,23 @@ class BestSellerListViewItem extends StatelessWidget {
                 ),
               ),
             ),
-          )
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -13,21 +13,24 @@ class HomeViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
-        Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: FeaturedBooksListView(),
-        ),
+        FeaturedBooksListView(),
         SizedBox(
           height: 50,
         ),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
+          padding: EdgeInsets.symmetric(horizontal: 30),
           child: Text(
             'Best Seller',
             style: Styles.textStyle18,
           ),
         ),
-        BestSellerListViewItem(),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: BestSellerListViewItem(),
+        ),
       ],
     );
   }
