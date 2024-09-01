69e4bdc9b6c474853827cdba7d9858e798b330ad
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-02 00:26:02 +0300

    search view done

--- lib/Features/search/presentaion/views/widgets/search_view_body.dart
+++ lib/Features/search/presentaion/views/widgets/search_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/presentaion/views/widgets/book_list_view_item.dart';
 import 'package:bookly/Features/search/presentaion/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,34 @@ class SearchViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomSearchTextField(),
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                Expanded(
+                  child: SearchResultListView(),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemCount: 6,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
