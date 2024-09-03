3182306feac9757e917f8636c4d071b4b1a29e8d
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-09-03 16:32:25 +0300

    create Featured Books cubit States

---
+++ lib/Features/home/presentaion/manger/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+
+
+
+}
